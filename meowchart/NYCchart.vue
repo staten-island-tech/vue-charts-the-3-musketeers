754579d8222421f3ce8df71e9bedc67e3f8e4043
RACJHEL FANN <124321963+rae-2@users.noreply.github.com>
2024-03-06 13:07:30 -0500

    meiw

--- meowchart/NYCchart.vue
+++ meowchart/NYCchart.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-
+        <h1>api url: https://data.cityofnewyork.us/resource/zt9s-n5aj.json?</h1>
     </div>
 </template>
 
@@ -10,6 +10,6 @@
     }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
 
 </style>
