dea03a5d1dd87cea067e173ce7de26fdd203ae39
RACJHEL FANN <124321963+rae-2@users.noreply.github.com>
2024-03-07 13:12:11 -0500

    Update NYCchart.vue

--- meowchart/NYCchart.vue
+++ meowchart/NYCchart.vue
@@ -5,7 +5,24 @@
 </template>
 
 <script>
-
+import {pie} from 'vue-chartjs'
+const data = {
+  labels: [
+    '280-380',
+    '381-550',
+    '551-736'
+  ],
+  datasets: [{
+    label: 'Number of Students',
+    data: [300, 50, 100],
+    backgroundColor: [
+      'rgb(255, 99, 132)',
+      'rgb(54, 162, 235)',
+      'rgb(255, 205, 86)'
+    ],
+    hoverOffset: 4
+  }]
+};
 </script>
 
 <style scoped>
