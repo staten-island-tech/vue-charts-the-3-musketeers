5b60110eae101c461231795aef2b47f0be9c8eaf
dieuhan <dieuhan.mut@gmail.com>
2024-03-12 13:16:07 -0400

    save and commit to github!

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-
+<h1>bleep bop</h1>
   </div>
 </template>
 
 <script setup>
-console.log("rachel says meow")
+console.log("rachel says meow");
+
+import { ref, onMounted } from 'vue';
+const NY = ref('')
+async function getNY(){
+  let res = await fetch('https://data.cityofnewyork.us/resource/zt9s-n5aj.json')
+  let data = await res.json()
+  NY.value = data
+  console.log(data);
+}
+onMounted(()=>{
+  getNY()
+});
+
+
+
+
+
 </script>
 
 <style lang="scss" scoped>
