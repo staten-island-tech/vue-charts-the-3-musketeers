75cb629a7fbfd0ee99c1bca9b812506271c0ade4
dieuhan <dieuhan.mut@gmail.com>
2024-03-21 13:09:36 -0400

    blji

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
 <h1>bleep bop</h1>
+<canvas id="myChart"></canvas>
   </div>
 </template>
 
@@ -26,27 +27,11 @@ onMounted(()=>{
   getNY()
 });
 
-
 const ctx = document.getElementById('myChart');
 
-new ChartJShart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
+new 
+
+
 
 
 </script>
