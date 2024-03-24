1f9cc30ccbc9bb0df07267afb857d997500c345f
dieuhan <dieuhan.mut@gmail.com>
2024-03-24 13:14:14 -0400

    i

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -3,6 +3,7 @@
 
 <h1>Chart of NYC Writing SAT Scores in 2010</h1>
 <canvas ref="pieChart" width="400" height="400"></canvas>
+
   </div>
 </template>
 
@@ -80,83 +81,42 @@ function process(data) {
         '#4BC0C0',
         '#9966FF',
         '#FF9900',
-        '#0099CC'
+        '#0099CC', //find real good colors
       ]
     }]
   };
 
-  for (let range in scoreRanges) {
+  for (let range in percentages) {
     chartData.labels.push(range);
     chartData.datasets[0].data.push(scoreRanges[range]);
   }
-
   console.log(chartData);
+  return (chartData);
 }
 
 onMounted(() => {
   getNY();
 });
 
-
-
-
-
-
-
-
-
-
-
-
-/* const NY = ref('')
-async function getNY(){
-  let res = await fetch('https://data.cityofnewyork.us/resource/zt9s-n5aj.json')
-  let data = await res.json()
-  NY.value = data
-  console.log(data);
-}
-onMounted(()=>{
-  getNY().then(() => {
-    generatePieChart();
-  });
-});
-
-function generatePieChart() {
-  const scores = NY.value.map(school => parseInt(school.writing_score));
-
-
-  const scoreRanges = {
-    '300-350': 0,
-    '351-400': 0,
-    '401-450': 0,
-    '451-500': 0,
-    '501-550': 0,
-    '551-600': 0,
-    '601-650': 0
-  };
-
-  scores.forEach(score => {
-    if (score >= 300 && score <= 350) {
-      scoreRanges['300-350']++;
-    } else if (score >= 351 && score <= 400) {
-      scoreRanges['351-400']++;
-    } else if (score >= 401 && score <= 450) {
-      scoreRanges['401-450']++;
-    } else if (score >= 451 && score <= 500) {
-      scoreRanges['451-500']++;
-    } else if (score >= 501 && score <= 550) {
-      scoreRanges['501-550']++;
-    } else if (score >= 551 && score <= 600) {
-      scoreRanges['551-600']++;
-    } else if (score >= 601 && score <= 650) {
-      scoreRanges['601-650']++;
-    }
-  });
-}
- */
-
-
 </script>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <style lang="scss" scoped>
 
