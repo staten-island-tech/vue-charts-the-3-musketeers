2d5c4beab71c388a19e79f20f61557eb4e518f60
RACJHEL FANN <124321963+rae-2@users.noreply.github.com>
2024-03-25 13:07:42 -0400

    meowowooeoeoe

--- meowchart/src/views/HomeView.vue
+++ meowchart/src/views/HomeView.vue
@@ -1,28 +1,13 @@
 <template>
     <div>
-        <canvas id="mychart"></canvas>
 
     </div>
 </template>
 
 <script setup>
-import {ref, onBeforeMount} from "vue"
-import { Pie } from "vue-chartjs"
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 
-/* const math = ref('')
-async function getMaths(){
-    let res = await fetch('https://data.cityofnewyork.us/resource/zt9s-n5aj.json?')
-    let data = await res.json(
-        math.value=data.results
-    )
-}
-onBeforeMount(() => {
-  getMaths();
-}); */
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
