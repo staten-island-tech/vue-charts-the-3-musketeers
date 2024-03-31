a6180f56a51513875651d4eeea308f376c80e31b
Seebso <124058253+Seebso@users.noreply.github.com>
2024-03-31 18:02:19 -0400

    donemy

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,28 +1,53 @@
 <script setup>
-import {ref} from 'vue'
-import {useRouter} from 'vue-router'
+import { ref, onBeforeMount, onMounted } from 'vue'
+import {useRoute} from 'vue-router'
+const schoolNames = ref('')
+const schools = ref('')
 const searchValue = ref('staten')
-const router = useRouter()
-const search = ()=>{
-  router.push({
-    name:"english",
-    query: {value: searchValue.value}
-  })
+const route = useRoute()
   
-  
-}
+
+const getsat = async()=>{
+ let response = await fetch("https://data.cityofnewyork.us/resource/zt9s-n5aj.json");
+     let data = await response.json();
+     
+
+     schoolNames.value = data.map(school =>{ 
+      return school.school_name ? school.school_name : ""});
+     schools.value = data;
+     /* chartData.value = {
+      labels: schoolNames,
+      datasets: [
+        {
+          label: 'Math!',
+          data: mathScores,
+          backgroundColor: '#f87979'
+        }
+      ]
+    } */
+ }
+ onBeforeMount(()=>{
+   /* getsat(); */
+   /* chartData.value = {
+        labels: [ 'January', 'February', 'March'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979'  ,
+            data: [40, 20, 12]
+          }
+        ]  
+      } */
+ })
+ 
+  getsat();
 
 </script>
 
 <template>
-  <div class="card" v-if="data" v-for="data in data">
-  <h1>{{ data.school_name }}</h1>
-</div>
-<div style="height: 400px; width: 400px">
-<label for="searchfield">
-  search for school:
-</label>
-<input id="searchfield" v-model="searchValue"/>
-<button @click="search">search</button>
-</div> 
+  <div>
+    <ol>
+      <li v-for="school in schools">{{ school.school_name }}</li>
+    </ol>
+  </div>
 </template>
